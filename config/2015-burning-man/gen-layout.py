20c8ebc29425e04562884a2272de394b9cb6c23c
Marcin Kaszynski <marcin.kaszynski@gmail.com>
2015-08-29 13:21:25 -0700

    Simplify config generator.

--- config/2015-burning-man/gen-layout.py
+++ config/2015-burning-man/gen-layout.py
@@ -22,24 +22,32 @@ COLOR_LAYOUT = "rgb"
 def print_out(pixels):
     for y in range(ROWS):
         for p in pixels[y * COLS:y * COLS + COLS]:
-            print "%d:%03d" % tuple(p[1:]),
+            print "%d:%03d" % tuple(p),
         print
 
 def pos(x, y):
     return x + y * COLS
 
+# Build an array:
+#
+# led_idx: [strand_idx, opc_led_idx]
+#
+# - led_idx is the index of led in natural order (row_idx * COLS + col_idx)
+# - opc_led_idx is the index of that led in OPC protocol
 def gen_pixels():
-    ret = [[i, 0, 0] for i in range(LED_CNT)]
+    ret = [[0, 0] for i in range(LED_CNT)]
 
     for l in range(STRANDS):
+        # Fadecandy constant: the first led on strand n has index 64*n
+        STRAND_START = 64 * l
         for x in range(COLS):
             p = ret[pos(x, 2 * l)]
-            p[1] = l
-            p[2] = l * 64 + x
+            p[0] = l
+            p[1] = STRAND_START + x
     
             p = ret[pos(COLS - x - 1, 2 * l + 1)]
-            p[1] = l
-            p[2] = l * 64 + x + COLS
+            p[0] = l
+            p[1] = STRAND_START + x + COLS
 
     return ret
 
@@ -66,7 +74,7 @@ FCSERVER_CONFIG_TEMPLATE = """
 
 def write_fcserver_config(fname, listen_addr, pixels):
     map_str = ",\n".join("[ %d, %d, %d, %d, \"%s\"]"
-                         % (0, p[0], p[2], 1, COLOR_LAYOUT)
+                         % (0, i, p[1], 1, COLOR_LAYOUT)
                  for (i, p) in enumerate(pixels))
     f = open(fname, 'w')
     f.write(FCSERVER_CONFIG_TEMPLATE % locals())
